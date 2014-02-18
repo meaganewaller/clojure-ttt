63f75973688513eb0b1946ab35c130870de886c9
Meagan Waller <meaganewaller@gmail.com>
2014-02-17 19:25:27 -0600

    finds empty spaces on the board

--- spec/clojure_ttt/board_spec.clj
+++ spec/clojure_ttt/board_spec.clj
@@ -7,3 +7,9 @@
               (should= [nil nil nil
                         nil nil nil
                         nil nil nil] (new-board))))
+
+(describe "available-spaces"
+          (it "returns all the empty spaces"
+              (should= 9 (count (available-spaces (new-board))))
+              (should= #{0 1 2 3 4 5} (available-spaces '(nil nil nil nil nil nil "O" "X" "X")))))
+
--- src/clojure_ttt/board.clj
+++ src/clojure_ttt/board.clj
@@ -1,7 +1,14 @@
 (ns clojure-ttt.board
   (:use clojure.set))
 
+(defn find-spaces [marker board]
+  (into #{} (keep-indexed #(if (= marker %2) %1) board)))
+
+(defn available-spaces [board]
+  (find-spaces nil board))
+
 (defn new-board []
   [nil nil nil
    nil nil nil
    nil nil nil])
+
